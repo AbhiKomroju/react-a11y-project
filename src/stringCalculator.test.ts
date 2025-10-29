7e82a0a64a4809af2a83d60e00aae7f6292b4973
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 22:40:04 +0530

    test: add test for custom delimiters (RED)

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -25,4 +25,9 @@ describe("String Calculator", () => {
     expect(add("1\n2,3")).toBe(6);
     expect(add("5\n6\n7")).toBe(18);
   });
+
+  it("should handle custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//;\n1;2;3")).toBe(6);
+  });
 });
