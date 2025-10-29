8c32cda15550901323235871304c421d218e3c77
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 22:21:23 +0530

    test: add test for two comma-separated numbers (RED)

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -5,8 +5,14 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
   it("should return the number itself if it is a single number", () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
+  });
+
+  it("should return the sum of two comma separated numbers", () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("5,6")).toBe(11);
   });
 });
