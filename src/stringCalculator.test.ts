799689e0464b0d7df48312c313b31a18d279a297
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 22:17:16 +0530

    test: add test for single number (RED)

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -5,4 +5,8 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  it("should return the number itself if it is a single number", () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+  });
 });
