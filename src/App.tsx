cb6517f17c6347f325aaad3c73ed1a9ef24e1770
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 23:10:03 +0530

    a11y: add descriptive alt text to string image

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => {
     <div style={{ padding: "20px", backgroundColor: "#fff", color: "#aaa" }}>
       <img
         src="https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="A piece of string on a surface, representing the string calculator concept"
         width={600}
         height={400}
       />
