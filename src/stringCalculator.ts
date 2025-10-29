714836db4de38a3f74f5f304c07a0032a2c9762c
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 22:47:36 +0530

    feat: support custom delimiters (GREEN)

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,7 +3,19 @@ export function add(numbers: string): number {
     return 0;
   }
 
-  const nums = numbers.split(/[\n,]/);
+  let numberString = numbers;
+
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    const customDelimiter = numbers.substring(2, delimiterEndIndex);
+    numberString = numbers.substring(delimiterEndIndex + 1);
+
+    while (numberString.includes(customDelimiter)) {
+      numberString = numberString.replace(customDelimiter, ",");
+    }
+  }
+
+  const nums = numberString.split(/[\n,]/);
   const sum = nums.reduce((acc, curr) => acc + parseInt(curr), 0);
   return sum;
 }
