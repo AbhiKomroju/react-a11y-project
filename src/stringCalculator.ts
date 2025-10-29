2a3202ef87432fdbd763960e4fca023bfb68bc6a
AbhiKomroju <abhikomroju@gmail.com>
2025-10-29 22:24:33 +0530

    feat: handle comma-separated numbers (GREEN)

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,5 +2,7 @@ export function add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
-  return parseInt(numbers);
+  const nums = numbers.split(",");
+  const sum = nums.reduce((acc, curr) => acc + parseInt(curr), 0);
+  return sum;
 }
